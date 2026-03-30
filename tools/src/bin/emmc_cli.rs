9bd8a23ba00f32b232d48b6b3f5ccbff379e2c12
tuxuser <462620+tuxuser@users.noreply.github.com>
2026-03-30 17:15:03 +0200

    feat/emmc_cli: Implement setting start page and count

--- tools/src/bin/emmc_cli.rs
+++ tools/src/bin/emmc_cli.rs
@@ -7,19 +7,37 @@ use libaspect2::spi::emmc_reader::EmmcReader;
 use std::fs::File;
 use std::io::{Read, Write};
 use std::path::PathBuf;
+use sdmmc_core::register::{ExtCsd, Cid};
 
 const MAX_NAND_PAGES: u32 = 0x9E0000;
 const BLOCK_SIZE: usize = 512;
 const CHUNK_PAGES: u32 = 128; // read 128 pages (64 KB) per CMD18
 
+fn parse_maybe_hex(arg: &str) -> Result<u64, std::num::ParseIntError> {
+    if arg.starts_with("0x") || arg.starts_with("0X") {
+        u64::from_str_radix(&arg[2..], 16)
+    }
+    else {
+        u64::from_str_radix(arg, 10)
+    }
+}
+
 #[derive(Parser, Clone, PartialEq, Eq, Debug)]
 struct FileOptions {
     /// File to read / write
     file: PathBuf,
+    /// Offset to start reading, beginning if not provided
+    #[arg(value_parser = parse_maybe_hex)]
+    offset: u64,
+    /// Length to read, until EOF is not provided
+    #[arg(value_parser = parse_maybe_hex)]
+    length: u64,
 }
 
 #[derive(Subcommand, Clone, PartialEq, Debug)]
 enum Command {
+    /// Get eMMC info
+    Info,
     /// Read eMMC
     Read(FileOptions),
     /// Write eMMC
@@ -58,7 +76,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             reader.dump_fuses()?;
             return Ok(());
         }
-        Command::Read(opts) => {
+        Command::Info => {
             // Initialize the device
             println!("Initializing device...");
             if let Err(e) = reader.init() {
@@ -67,14 +85,44 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
             println!("\nDevice initialized successfully!");
 
-            let total_bytes = MAX_NAND_PAGES as u64 * BLOCK_SIZE as u64;
+            let mut ext_csd = [0u8; 512];
+            reader.read_ext_csd(&mut ext_csd)?;
+            ext_csd.reverse();
+
+            let cid = reader.cid().clone();
+            let cid_bytes: Vec<u8> = [cid[3].to_be_bytes(), cid[2].to_be_bytes(), cid[1].to_be_bytes(), cid[0].to_be_bytes()].as_flattened().to_vec();
+            let cid_parsed = Cid::try_from_bytes(&cid_bytes);
+            let ext_csd_parsed = ExtCsd::try_from_inner(ext_csd);
+            println!("CID: {cid_parsed:?}");
+            println!("EXT CSD: {ext_csd_parsed:?}");
+        }
+        Command::Read(opts) => {
+            // Initialize the device
+            println!("Initializing device...");
+            if let Err(e) = reader.init() {
+                return Err(anyhow::anyhow!("Error initializing device: {}", e).into());
+            }
+
+            let start_page = match opts.offset {
+                0 => 0,
+                other => other / BLOCK_SIZE as u64
+            };
+
+            let page_count = match opts.length {
+                0 => MAX_NAND_PAGES as u64 - start_page,
+                other => other / BLOCK_SIZE as u64
+            };
+
+            println!("\nDevice initialized successfully!");
+
+            let total_bytes = page_count * BLOCK_SIZE as u64;
             let mut file = File::create(opts.file)?;
             let mut chunk_buf = vec![0u8; CHUNK_PAGES as usize * BLOCK_SIZE];
 
             // Read eMMC pages
             println!(
                 "Reading eMMC ({} pages, {:.2} GiB)...",
-                MAX_NAND_PAGES,
+                page_count,
                 total_bytes as f64 / (1024.0 * 1024.0 * 1024.0)
             );
 
@@ -85,9 +133,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                     .unwrap()
             );
 
-            let mut page = 0u32;
-            while page < MAX_NAND_PAGES {
-                let remaining = MAX_NAND_PAGES - page;
+            let mut page = start_page as u32;
+            while page < (start_page + page_count) as u32 {
+                let remaining = ((start_page + page_count) - page as u64) as u32;
                 let count = remaining.min(CHUNK_PAGES);
                 let byte_count = count as usize * BLOCK_SIZE;
 
